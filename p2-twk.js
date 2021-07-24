e2894ff37104ac79bfbb885c665abdc9d5dbf5d8
wbgits <57925703+wbgits@users.noreply.github.com>
2021-07-25 05:20:30 +0700

    Add files via upload

---
+++ p2-twk.js
@@ -0,0 +1,316 @@
+window.DATA_QUESTION_TWK = (()=>{
+	const p_stringTrim = (()=>{
+		const reg_newLine = /\n/g;
+		const reg_doubleSpace = /\s\s+/g;
+		return (s)=>{
+			return s.replace(reg_newLine, " ").replace(reg_doubleSpace, " ").trim();
+		};
+	})();
+	const stringQuestions = `Secara ideal, kerukunan hidup yang terjadi di antara pemeluk agama di Indonesia atas dasar kebersamaan harus selalu dibina dan dikembangkan agar...
+///Dapat menyelesaikan tantangan globalisasi
+///Tercapai kesejahteraan bagi seluruh rakyat Indonesia.
+///Perdamaian terjaga dengan baik.
+///Konflik bisa dicegah.
+///Persatuan dan kesatuan bangsa dapat dicapai.
+///e 
+
+///Berikut ini adalah sikap-sikap yang mengutamakan kepentingan umum dibandingkan kepentingan pribadi dan golongan, kecuali...
+///Menggunakan fasilitas umum semaksimal mungkin.
+///Selalu menolong orang yang sedang kesulitan walaupun tidak diminta.
+///Berusaha menjaga kebersihan di wilayah sekitar RT.
+///Menyelesaikan persoalan rumah tangga dari tetangga,
+///Membudayakan tertib antri.
+///d 
+
+///Salah satu periode demokrasi yang telah dilalui oleh Indonesia adalah masa demokrasi parlementer atau demokrasi liberal yang ditunjukkan dengan...
+///Dominannya perang militer, presiden sebagai kepala negara, pemerintahan berpusat di presiden.
+///Pemerintahan berpusat di presiden, adanya perdana Menteri, Kabinet bertanggung jawab kepada parlemen.
+///Dominannya perang militer, adanya perdana Menteri, Kabinet bertanggung jawab kepada parlemen.
+///Presiden sebagai kepala negara, adanya perdana Menteri, Kabinet bertanggung jawab kepada parlemen.
+///Kabinet bertanggung jawab kepada parlemen, pemerintahan berpusat di presiden.
+///d
+
+///Pencapaian tertinggi dalam pesta demokrasi dicapai Indonesia Ketika berhasil melaksanakan pemilu presiden secara langsung oleh seluruh rakyat pada tahun...
+///2004
+///1999
+///2009
+///1971
+///1955
+///a 
+
+///Pemegang kekuasaan tertinggi dalam sistem demokrasi adalah...
+///Majelis Perwakilan rakyat
+///Dewan Senat
+///Parlemen
+///Rakyat
+///Dewan Legislatif
+///d 
+
+///Terdapat beberapa aspek yang menjadi latar belakang wawasan nusantara, manakah yang bukan merupakan latar aspek-aspek tersebut?
+///Aspek kesejahteraan
+///Aspek sosial-Budaya
+///Aspek kekeluargaan
+///Aspek falsafah Pancasila
+///Aspek kewilayahan
+///c
+
+///Negara tetangga yang berbatasan langsung dengan Indonesia adalah...
+///Malaysia, Timor Leste, Papua Nugini
+///Papua Nugini, Filipina, Chili
+///Malaysia, India, Selandia Baru
+///Timor Leste, Singapura, Fiji
+///Singapura,Vietnam, Brunei Darussalam
+///a 
+
+///Salah satu tujuan wawasan nusantara adalah menghendaki adanya persatuan dan kesatuan dalam kehidupan, sosial, maupun alamiah. Aspek kealamiahan yang dimaksud adalah...
+///Budaya lokal
+///Flora dan fauna
+///Ekonomi sosial
+///Pangan
+///Politik demokrasi
+///b 
+
+///Implementasi wawasan nusantara dalam bidang politik diyakini akan menciptakan...
+///Sikap batiniah dan lahiriah yang mengakui segala macam bentuk perbedaan.
+///Pemerintahan dan pengelolaan negara yang baik.
+///Munculnya cinta tanah air dan menumbuhkan bela negara.
+///Pendidikan politik yang baik secara internasional
+///Tatanan ekonomi yang baik secara politis
+///c 
+
+///Salah satu bentuk implementasi HAM terhadap orang lain yaitu...
+///Memberikan sumbangan kepada panitia acara-acara sosial
+///Membantu tetangga yang sedang kesusahan
+///Menyelamatkan anak-anak yang putus sekolah
+///Mempersilakan warga yang berbeda keyakinan untuk menjalankan ibadahnya
+///Melaporkan pelaku tindak kekerasan anak kepada pihak yang berwajib
+///d 
+
+///Deklarasi Umum tentang Hak-Hak Asasi Manusia yang diratifikasi sewaktu rapat umum PBB terjadi pada...
+///30 Agustus 1947
+///10 Desember 1948
+///10 Maret 1949
+///16 April 1950
+///5 Maret 1951
+///b 
+
+///Salah satu pelanggaran HAM berat disebut dengan istilah “Crime Against Humanity”, maksud dari istilah ini adalah...
+///Menghilangkan harta pusaka suatu suku bangsa
+///Membunuh orang atau kelompok tertentu
+///Mengusir sekelompok orang secara paksa
+///Menyakiti secara sistematik kelompok tertentu
+///Membuat sesuatu yang kemudian menyebabkan orang dari suatu daerah terpaksa pindah
+///d 
+
+///Sebagai sebuah sistem yang melindungi segenap manusia di dunia ini, HAM bisa dibagi menjadi dua jenis pelanggaran: berat dan ringan. Contoh pelanggaran HAM ringan adalah...
+///Penembakan jamaah masjid di wilayah Sinai
+///Tragedi London berdarah
+///Pencurian mobil
+///Pengusiran bangsa Rohingya
+///Pemberontakan bersenjata
+///c 
+
+///Jumlah bulu yang benar pada lambing Garuda Pancasila adalah...
+///Bulu pada ekor berjumlah 17, bulu pada sayap berjumlah 8, bulu di bawah perisai/pangkal ekor berjumlah 19, bulu di leher berjumlah 45
+///bulu di bawah perisai/pangkal ekor berjumlah 17, Bulu pada ekor berjumlah 8, bulu pada sayap berjumlah 19, bulu di leher berjumlah 45
+///bulu di leher berjumlah 17, bulu pada sayap berjumlah 8, bulu di bawah perisai/pangkal ekor berjumlah 19, Bulu pada ekor berjumlah 45
+///bulu pada sayap berjumlah 17, Bulu pada ekor berjumlah 8, bulu di leher berjumlah 19, bulu di bawah perisai/pangkal ekor berjumlah 45
+///bulu pada sayap berjumlah 17, Bulu pada ekor berjumlah 8, bulu di bawah perisai/pangkal ekor berjumlah 19, bulu di leher berjumlah 45
+///e 
+
+///DR. R. M. Ng. Poerbatjaraka mengusulkan lambing pada Pancasila yang hingga saat ini kita kenal, yaitu...
+///Padi dan kapas
+///Kepala banteng
+///Pohon beringin
+///Rantai
+///Perisai dan bintang 
+///c 
+
+///Lambang kepala banteng dari sila Pancasila diusulkan oleh...
+///Ki Hadjar Dewantara
+///Ir. Soekarno
+///Mohammad Yamin
+///Mohammad hatta
+///Sultan hamid II
+///c 
+
+///Berikut ini adalah unsur-unsur pembentuk nasionalisme Indonesia, kecuali...
+///Kesatuan asas kerohanian
+///Kesatuan nasib
+///Kesatuan wilayah
+///Kesatuan sejarah
+///Kesatuan konsep musyawarah
+///e 
+
+///Unsur hakikat manusia sebagai makhluk individu dan makhluk sosial di saat yang bersamaan disebut juga dengan istilah...
+///Majemuk
+///Makhluk sosial
+///Monopluralis
+///Individualitas
+///solitude
+///c
+
+///Rumusan Pancasila dalam versi orisinil dan resmi ada di dalam...
+///Kitab Negara Kertagama
+///Pidato Ir. Soekarno
+///Pidato Mohammad yamin
+///Piagam Jakarta
+///Pembukaan UUD 1945
+///e
+
+///Nilai-nilai yang dikandung oleh Pancasila bersifat mendasar dan tidak serta merta operasional, hal ini merupakan karakter Pancasila sebagai...
+///Pedoman bangsa Indonesia
+///Ideologi nasional
+///Ideologi terbuka
+///Ideologi pembangunan
+///Ideologi masa depan
+///c 
+
+///Sebagai materi inti di dalam Pembukaan Undang-Undang Dasar 1945, Pancasila termaktub pada Alinea ke...
+///Satu
+///Dua
+///Tiga
+///Empat
+///Lima 
+///d 
+
+///Sila yang dilambangkan sebagai sesuatu yang bisa memayungi secara keseluruhan dari ragam budaya yang ada di Indonesia adalah sila...
+///Pertama
+///Kedua
+///Ketiga
+///Keempat
+///kelima
+///c 
+
+///sebagai janji sucinya, Patih Gadjah Mada dari Majapahit mengucapkan sebuah sumpah yang isinya akan menaklukan sepuluh wilayah yang tercakup dalam Nusantara, salah satu saerah yang disebutkan adalah Tumasik, wilayah manakah itu?
+///Malaysia
+///Singapura
+///Riau
+///Sriwijaya
+///Sumbawa 
+///b 
+
+///Manakah kelompok negara asing yang pernah memerintah di wilayah Indonesia pada masa penjajahan?
+///Jepang, Jerman, Portugis
+///Inggris, Spanyol, Kroasia
+///Belanda, Perancis, Spanyol
+///Jepang, Belanda, Amerika Serikat
+///Rusia, Turki, swedia
+///c 
+
+///Amandemen UUD 1945 menyebabkan hilangnya salah satu Lembaga yaitu...
+///Komisi Yudisial
+///DPA
+///DPR
+///Mahkamah Agung
+///DPD
+///b 
+
+///Sistem pembangunan nasional merupakan satu rancangan pembangunan jangka Panjang yang diatur secara khusus di dalam...
+///UU Nomor 39 Tahun 1999
+///UU Nomor 25 Tahun 2004
+///UU Nomor 32 Tahun 2004
+///UU Nomor 12 Tahun 2008
+///UU Nomor 8 Tahun 2015
+///b 
+
+///Amandemen ke-2 UUD RI 1945 dilaksanakan pada...
+///14-21 Oktober 1999
+///7-18 agustus 2000
+///1-9 November 2001
+///1-11 Agustus 2002
+///11-21 Oktober 2003
+///b 
+
+///Berdasarkan pasal 4 UU Nomor 12 Tahun 2006 tentang Kewarganegaraan REpublik Indonesia, yang termasuk Warga Negara Indonesia atau WNI dijabarkan pada penyataan di bawah ini, kecuali...
+///Seorang anak yang lahir dari pernikahan yang sah seorang ayah dan ibu WNI.
+///Seorang anak yang lahir dari pernikahan yang sah dari seorang ayah WNI dan ibu WNA.
+///Seorang anak yang lahir dari pernikahan yang sah seorang ayah WNA dan ibu WNI.
+///Seorang anak yang lahir di wilayah negara Republik Indonesia yang pada waktu lahir tidak jelas kewarganegaraannya.
+///Seorang anak yang lahir dalam jangka waktu 300 hari setelah ayahnya meninggal dunia, dari perkawinan yang sah dan ayahnya WNA.
+///e 
+
+///Berikut ini adalah kewajiban-kewajiban negara terhadap warga negaranya, kecuali
+///Menjamin hak asasi warga negara
+///Menjamin warga negara dalam memiliki aset
+///Memberi kebebasan beribadah
+///Mengembangkan Pendidikan untuk rakyat
+///Memberikan jaminan sosial
+///b
+
+///Apabila nama-nama pemohon untuk menjadi warga negara telah disahkan, maka yang memiliki wewenang untuk mengumumkan dalam Berita Negara RI adalah...
+///Dirjen Imigrasi
+///Menteri
+///Presiden
+///Dirjen Kemenkumham
+///Hakim MK
+///b`;
+	
+	const returned = [];
+	const arrayStringQuestions = [];
+	const pieces = stringQuestions.split("///");
+	const alpabetIndex = ["a", "b", "c", "d", "e"];
+	const group = "TES WAWASAN KEBANGSAAN";
+	const groupAlias = "twk";
+	const len = pieces.length;
+	let currentIndex = 0;
+	let currentItem;
+	for(let i=0;i<len;i++){
+		currentIndex += 1;
+		const p = p_stringTrim(pieces[i]);
+		if(currentIndex===1){
+			currentItem = {};
+			currentItem.group = group;
+			currentItem.groupAlias = groupAlias;
+			currentItem.content = p;
+		}
+		else if(currentIndex < 7){ // index untuk opsi
+			if(currentIndex === 2){
+				currentItem.option = [];
+			}
+			currentItem.option.push(p);
+		}
+		else{ // scores
+			currentItem.scores = [];
+			for(let sIndex = 0;sIndex < 5;sIndex++){
+				if(p.toLowerCase() === alpabetIndex[sIndex]){
+					currentItem.scores.push(5);
+				}
+				else{
+					currentItem.scores.push(0);
+				}
+			}
+			returned.push(currentItem);
+			currentIndex = 0;
+		}
+	}
+
+	
+	
+	return returned;
+})();
+//console.log(window.DATA_QUESTION_TWK);
+/* AMBIL KUNCI JAWABAN */
+/*
+const kunciLabels = ["A", "B", "C", "D", "E"];
+const items = window.DATA_QUESTION_TWK;
+const len = items.length;
+const kunci = [];
+for(let i=0;i<len;i++){
+	const item = items[i];
+	const scores = item.scores;
+	for(let j=0;j<5;j++){
+		if(scores[j] === 5){
+			kunci.push(kunciLabels[j]);
+			break;
+		}
+	}
+}
+
+const info = [];
+for(let i=0;i<items.length;i++){
+	info.push(i + 1 + ". "+ kunci[i]);
+}
+
+print(info.join("\n"));
+*/
